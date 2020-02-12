c5fab5c2cbba4f0eb238569d8bf928d7ae949980
seanyen <seanyen@microsoft.com>
2020-02-12 13:22:09 -0800

    Initial windows bringup.

--- moveit_ros/warehouse/warehouse/include/moveit/warehouse/planning_scene_storage.h
+++ moveit_ros/warehouse/warehouse/include/moveit/warehouse/planning_scene_storage.h
@@ -43,6 +43,18 @@
 #include <moveit_msgs/MotionPlanRequest.h>
 #include <moveit_msgs/RobotTrajectory.h>
 
+// Import/export for windows dll's and visibility for gcc shared libraries.
+
+#ifdef ROS_BUILD_SHARED_LIBS // ros is being built around shared libraries
+  #ifdef moveit_warehouse_EXPORTS // we are building a shared lib/dll
+    #define MOVEIT_WAREHOUSE_DECL ROS_HELPER_EXPORT
+  #else // we are using shared lib/dll
+    #define MOVEIT_WAREHOUSE_DECL ROS_HELPER_IMPORT
+  #endif
+#else // ros is being built around static libraries
+  #define MOVEIT_WAREHOUSE_DECL
+#endif
+
 namespace moveit_warehouse
 {
 typedef warehouse_ros::MessageWithMetadata<moveit_msgs::PlanningScene>::ConstPtr PlanningSceneWithMetadata;
@@ -58,10 +70,10 @@ MOVEIT_CLASS_FORWARD(PlanningSceneStorage);
 class PlanningSceneStorage : public MoveItMessageStorage
 {
 public:
-  static const std::string DATABASE_NAME;
+  static MOVEIT_WAREHOUSE_DECL const std::string DATABASE_NAME;
 
-  static const std::string PLANNING_SCENE_ID_NAME;
-  static const std::string MOTION_PLAN_REQUEST_ID_NAME;
+  static MOVEIT_WAREHOUSE_DECL const std::string PLANNING_SCENE_ID_NAME;
+  static MOVEIT_WAREHOUSE_DECL const std::string MOTION_PLAN_REQUEST_ID_NAME;
 
   PlanningSceneStorage(warehouse_ros::DatabaseConnection::Ptr conn);
 
